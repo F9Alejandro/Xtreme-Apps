229af3dd2924c35245fee879a08660074b7d117a
Alexander Featherston <f91896@gmail.com>
2024-01-05 17:30:22 -0500

    Updated to use env to get newest bash version instead of hardcoded default system version.
     Changes to be committed:
    	modified:   .utils/add-subtree.sh
    	modified:   .utils/bulk-update-subtrees.sh
    	modified:   .utils/subtree-subdir-helper.sh
    	modified:   .utils/update-subtree.sh

--- .utils/update-subtree.sh
+++ .utils/update-subtree.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 if [ "$(git rev-parse --show-prefix)" != "" ]; then
@@ -16,11 +16,14 @@ while read repo branch subdir; do
     if [ "${repo:0:1}" = "#" ]; then
         continue
     fi
-    exec 69>&1
+    #exec 69>&1
+    tmpfile=$(mktemp /tmp/script.XXXXXX)
     if [ "${subdir}" = "/" ]; then
-        result="$(git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}" 2>&1 | tee /dev/fd/69)"
+        #result="$(git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}" 2>&1 | tee /dev/fd/69)"
+        result="$(git subtree pull -P "${path}" "${repo}" "${branch}" -m "Merge ${path} from ${repo}" 2>&1 | tee "$tmpfile")"
     else
-        result="$(bash .utils/subtree-subdir-helper.sh "${path}" "${repo}" "${branch}" "${subdir}" merge 2>&1 | tee /dev/fd/69)"
+        #result="$(bash .utils/subtree-subdir-helper.sh "${path}" "${repo}" "${branch}" "${subdir}" merge 2>&1 | tee /dev/fd/69)"
+        result="$(bash .utils/subtree-subdir-helper.sh "${path}" "${repo}" "${branch}" "${subdir}" merge 2>&1 | tee "$tmpfile")"
     fi
     if grep "Automatic merge failed; fix conflicts and then commit the result." <<< "$result" > /dev/null; then
         echo "MERGE_MSG: Merge ${path} from ${repo}"
@@ -30,4 +33,5 @@ while read repo branch subdir; do
             sleep 1
         done
     fi
+    rm $tmpfile
 done < "${path}/.gitsubtree"
